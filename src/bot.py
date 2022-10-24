502a3143e0bb85d09dcacd9885f17b6d2849e811
sushi-chaaaan <sushi_code@outlook.jp>
2022-10-25 03:41:46 +0900

    軽微な加筆修正

--- src/bot.py
+++ src/bot.py
@@ -7,24 +7,23 @@ from tools.io import read_yaml
 
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
+        # load config files
         self.config = ConfigYaml(**read_yaml(r"config/config.yaml"))
-        self.__dotenv_path = f".env.{self.config.Environment}"
-        self.env = DotEnv(_env_file=self.__dotenv_path)  # type: ignore
+        self.env = DotEnv(_env_file=f".env.{self.config.Environment}")  # type: ignore
 
         super().__init__(command_prefix=self.config.CommandPrefix, **kwargs)
 
     async def setup_hook(self) -> None:
         if self.config.ClearAppCommands:
-            await self.clear_app_commands()
-            return
+            await self.clear_app_commands_and_close()
 
-        await self.load_cog(reload=False)
+        await self.load_cogs()
         await self.sync_app_commands()
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         pass
 
-    async def load_cog(self, reload: bool = False):
+    async def load_cogs(self, reload: bool = False) -> None:
         for ext in self.config.Extensions:
             try:
                 if reload:
@@ -38,10 +37,9 @@ class Bot(commands.Bot):
                 # self.logger.info(f"Loaded extension {ext}")
                 pass
 
-    async def sync_app_commands(self):
-
+    async def sync_app_commands(self) -> None:
         try:
-            await self.tree.sync(guild=self.app_command_sync_target)
+            await self.tree.sync(guild=self.app_commands_sync_target)
         except Exception as e:
             # self.logger.error("Failed to sync application commands", exc_info=e)
             pass
@@ -49,16 +47,17 @@ class Bot(commands.Bot):
             # self.logger.info("Application commands synced successfully")
             pass
 
-    async def clear_app_commands(self):
-        self.tree.clear_commands(guild=None)
-        await self.tree.sync(guild=None)
-        await self.close()
-
     async def setup_view(self):
         pass
 
+    async def clear_app_commands_and_close(self) -> None:
+        self.tree.clear_commands(guild=None)
+        await self.tree.sync(guild=None)
+        await self.close()
+        return
+
     @property
-    def app_command_sync_target(self) -> Object | None:
+    def app_commands_sync_target(self) -> Object | None:
         target: Object | None = None
         if self.config.SyncGlobally:
             pass
