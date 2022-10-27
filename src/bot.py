e7e33ae7ecb0eae5b7da469783e4117fd4321274
sushi-chaaaan <sushi_code@outlook.jp>
2022-10-27 22:49:27 +0900

    不要なignoreを削除

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands  # type: ignore
+from discord.ext import commands
 
 from schemas.config import ConfigYaml, DotEnv
 from schemas.ui import PersistentView
