feaed22534aaadd68ee78f214814b729b62a05f3
sushi-chaaaan <mail@sushichan.live>
2022-12-15 06:59:51 +0900

    型アノテーションの修正

--- utils/run_any.py
+++ utils/run_any.py
@@ -2,7 +2,7 @@ import asyncio
 from typing import Any, Callable
 
 
-async def call_any_func(func: Callable, *args: Any, **kwargs: Any) -> Any:
+async def call_any_func(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
     if asyncio.iscoroutinefunction(func):
         return await func(*args, **kwargs)
     return func(*args, **kwargs)
