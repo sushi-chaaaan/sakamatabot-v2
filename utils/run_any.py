83467c204ff63a0493a9efa7a020a08f4fb9b588
sushi-chaaaan <mail@sushichan.live>
2022-12-15 22:19:12 +0900

    test view

--- utils/run_any.py
+++ utils/run_any.py
@@ -3,9 +3,6 @@ from typing import Any, Callable
 
 
 async def call_any_func(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
-    if func is None:
-        return None
-
     if asyncio.iscoroutinefunction(func):
         return await func(*args, **kwargs)
     return func(*args, **kwargs)
