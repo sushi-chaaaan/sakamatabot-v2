f1da1fd2b83aba6d8f9dc659ee023d3f84a55f93
sushi-chaaaan <mail@sushichan.live>
2022-12-20 21:45:39 +0900

    allow all mention

--- src/extensions/utils.py
+++ src/extensions/utils.py
@@ -69,8 +69,10 @@ class Utils(commands.Cog):
     @app_commands.command(name="mention_role", description="role mention test")
     @app_commands.guilds(discord.Object(id=int(os.environ["GUILD_ID"])))
     async def mention_role(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"<@&{self.bot.env.ADMIN_ROLE_ID}>")
-        await interaction.followup.send(f"<@&{self.bot.env.ADMIN_ROLE_ID}>")
+        await interaction.response.send_message(
+            f"<@&{self.bot.env.ADMIN_ROLE_ID}>", allowed_mentions=discord.AllowedMentions.all()
+        )
+        await interaction.followup.send(f"<@&{self.bot.env.ADMIN_ROLE_ID}>", allowed_mentions=discord.AllowedMentions.all())
         return
 
 
