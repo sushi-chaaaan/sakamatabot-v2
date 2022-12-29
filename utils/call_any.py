5006b2b53e0e8ecc65277f022ccd8bd1b48067c9
sushi-chaaaan <sushi_code@outlook.jp>
2022-12-29 22:44:39 +0900

    call_any 修正

--- utils/call_any.py
+++ utils/call_any.py
@@ -2,7 +2,7 @@ import asyncio
 from typing import Any, Callable
 
 
-def call_any_func(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
+async def call_any_func(func: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
     if asyncio.iscoroutinefunction(func):
-        return asyncio.run(func(*args, **kwargs))
+        return await func(*args, **kwargs)
     return func(*args, **kwargs)
